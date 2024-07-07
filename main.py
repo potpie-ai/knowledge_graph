a41ac1a712273b60e35159008ca0e9a0edc9d528
Vineet <vn.sh@outlook.com>
2024-07-07 20:11:39 +0530

    docker compose integration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from flow import understand_flows
 from knowledge_graph import KnowledgeGraph
 from dotenv import load_dotenv
 
@@ -8,14 +7,19 @@ load_dotenv(verbose=True, override=True)
 
 app = FastAPI()
 class QueryRequest(BaseModel):
-    project_id: str
+    project_id: int
     query: str
 
 @app.post("/query")
 async def query_knowledge_graph(request: QueryRequest):
-    knowledge_graph = KnowledgeGraph(request.project_id)
-    result = knowledge_graph.query(request.query, request.project_id)
-    return {"result": result}
+    try:
+        knowledge_graph = KnowledgeGraph(request.project_id)
+        result = knowledge_graph.query(request.query, request.project_id)
+        print("returning result",result)
+        return {"result": result}
+    except Exception as e:
+        print("error in query_knowledge_graph",e)
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
