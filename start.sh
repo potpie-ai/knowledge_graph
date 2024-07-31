843b228471704ab0c87cbd84af332bee532cf6c6
Vineet <vn.sh@outlook.com>
2024-07-31 21:06:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+source .env
+
 # Start celery worker and main application
 echo "Starting Celery worker..."
-celery --app=inferflow worker -l INFO --pool solo -Q dev &
+celery --app=inferflow worker -l INFO --pool solo -Q ${CELERY_QUEUE_NAME} &
 
 echo "Starting main application..."
 python3 main.py &
